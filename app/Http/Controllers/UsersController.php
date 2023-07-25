64d774ba9d2b0ad4da6479d4cdd71e9acf9b808a
edwin <metoohu@126.com>
2023-07-25 15:09:15 +0800

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller
         $data = $request->all();
         //如果存在图片上传
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id,416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
