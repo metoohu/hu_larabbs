826acf579c4303c53bc91ab2ef1049df87bae9d4
edwin <metoohu@126.com>
2023-08-01 14:26:27 +0800

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'LaraBBS') - edwin学习</title>
-
+  <title>@yield('title', 'LaraBBS') - edwin学习社区</title>
+  <meta name="description" content="@yield('description', 'edwin学习社区')" />
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   @yield('styles')
