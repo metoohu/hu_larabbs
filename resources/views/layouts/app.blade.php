9a365310bc879e8fc1bd9db29897ab2dfac433b6
edwin <metoohu@126.com>
2023-08-08 16:35:09 +0800

    站点设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,14 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'LaraBBS') - edwin学习社区</title>
-  <meta name="description" content="@yield('description', 'edwin学习社区')" />
+  <title>@yield('title', 'Edwin 博客') - {{ setting('site_name', 'Edwin 博客') }}</title>
+  <meta name="description" content="@yield('description', setting('seo_description', 'Edwin 博客'))" />
+  <meta name="keywords" content="@yield('keyword', setting('seo_keyword', 'Edwin 博客,Edwin 博客'))" />
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   @yield('styles')
